216df955b23849568db5928b9e970abd77a348d6
sleepingnext <veroskevin8@gmail.com>
2021-12-14 00:42:29 +0700

    feature: add login and logout function

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -2,19 +2,48 @@
 
 namespace App\Controllers;
 
+use App\Models\UserModel;
+
 class User extends BaseController
 {
     public function login()
     {
-        $request = service('request');
-        if ($request->getMethod() == "get") {
+        if ($this->request->getMethod() == "get") {
             $data = [
                 "title" => "Login",
             ];
 
             return view("user/login", $data);
-        } else if ($request->getMethod() == "post") {
-            return "";
+        } else if ($this->request->getMethod() == "post") {
+            $session = session();
+
+            $userModel = new UserModel();
+            $username = $this->request->getVar("username");
+            $password = hash( "sha256", $this->request->getVar("password"));
+
+            $data = $userModel->where("username", $username)->first();
+
+            if (isset($data)) {
+                if ($data["password"] == $password) {
+                    $session->set([
+                        "user_id" => $data["id"],
+                        "username" => $data["username"],
+                    ]);
+
+                    return redirect()->to("/product/index");
+                } else {
+                    $session->setFlashdata("error_message", "Password is incorrect.");
+                    return redirect()->to("/user/login", null, "get");
+                }
+            } else {
+                $session->setFlashdata("error_message", "Email does not exist.");
+                return redirect()->to("/user/login", null, "get");
+            }
         }
+    }
+
+    public function logout()
+    {
+        session()->remove(["user_id", "username"]);
     }
 }
--- app/Views/user/login.php
+++ app/Views/user/login.php
@@ -2,12 +2,12 @@
 
 <?= $this->section("content"); ?>
     <div class="container-fluid">
-        <form action="/user/login">
+        <form action="/user/login" method="post">
             <div class="row">
                 <div class="col-4 mb-4 offset-4">
                     <label for="username" class="form-label">Username</label>
                     <div class="input-group">
-                        <input type="text" class="form-control form-control-lg" id="username">
+                        <input type="text" class="form-control form-control-lg" id="username" name="username">
                     </div>
                 </div>
             </div>
@@ -15,7 +15,7 @@
                 <div class="col-4 mb-4 offset-4">
                     <label for="password" class="form-label">Password</label>
                     <div class="input-group">
-                        <input type="password" class="form-control form-control-lg" id="password">
+                        <input type="password" class="form-control form-control-lg" id="password" name="password">
                     </div>
                 </div>
             </div>
